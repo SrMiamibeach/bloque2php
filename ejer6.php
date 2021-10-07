3175fc0bb7fbac84bfa7dd2d7530df88b311e6bb
Aaron <agomezdo20dw@ikzubirimanteo.com>
2021-10-07 13:25:53 +0200

    Ejercicio 6 hecho

--- ejer6.php
+++ ejer6.php
@@ -1,67 +1,107 @@
+
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Ejercicio5</title>
+    <title>Ejercicio6</title>
+    <style>
+        #contenedor1{
+            display: flex;
+            flex-direction: row;
+        }
+        h4{
+            color: blue;
+            width: 8%;
+            display: flex;
+            flex-direction: column;
+        }
+        #contenedor2{
+            display: flex;
+            flex-direction: row;
+        }
+        #nombres{
+            width: 8%;
+            display: flex;
+            flex-direction: column;
+
+        }
+    </style>
 </head>
 
 <body>
     <form method="POST">
         <label>Introduce un mes y te diremos cuantos dias tiene</label>
-        <p>Introduce un nombre:<input type="text" name="nombre" /></p>
+        <p>Introduce un nombre:<input type="text" name="nombre" htmlentities/></p>
         <p>Introduce un mes:<input type="text" name="mes" /></p>
-        <input type="submit" value="Enviar" />
+        <input type="submit" value="Enviar" name="submit" />
     </form>
     <?php
-    class ejer6
-    {
+    if (!isset($_COOKIE['cumples'])) {
+        $cumples = array(
+            "enero"      => array(),
+            "febrero"    => array(),
+            "marzo"      => array(),
+            "abril"      => array(),
+            "mayo"       => array(),
+            "junio"      => array(),
+            "julio"      => array(),
+            "agosto"     => array(),
+            "septiembre" => array(),
+            "octubre"    => array(),
+            "noviembre"  => array(),
+            "diciembre"  => array(),
+        );
+        setcookie('cumples', json_encode($cumples), 0);
+    } else {
+        $cumples = json_decode($_COOKIE['cumples'], true);
+    }
 
-        private $array;
-        public function __construct()
-        {
-            $this->array =[
-                "enero" => "",
-                "febrero" => "",
-                "marzo" => "",
-                "abril" => "",
-                "mayo" => "",
-                "junio" => "",
-                "julio" => "",
-                "agosto" => "",
-                "septiembre" => "",
-                "octubre" => "",
-                "noviembre" => "",
-                "diciembre" => "",
-            ];
+    function anadirNombre($nombre, $mes) {
+        global $cumples;
+        array_push($cumples[$mes], $nombre);
+        setcookie('cumples', json_encode($cumples), 0);
+        $cant_personas = 0;
+        foreach($cumples as $mes => $nombres){
+            $cant_personas += count($nombres);
         }
-        public function anadirNombre($nombre, $mes)
-        {
-            $nombres = $this->array[$mes];
-            echo "A".$nombres;
-            $this->array[$mes] = $nombres . $nombre.",";
+        return $cant_personas;
+    }
+
+    function mostrar() {
+        global $cumples;
+        echo '<div id="contenedor1">';
+        foreach($cumples as $mes => $nombres){
+            echo '<h4>'.$mes.'</h4>';
         }
-        public function mostrar()
-        {
-            foreach($this->array as $key => $value){
-                echo $key."--".$value."<br>";
+        echo '</div>';
+        echo '<div id="contenedor2">';
+        foreach($cumples as $mes => $nombres){
+            $texto="";
+            foreach($nombres as $nombre) {
+                $texto.=$nombre.'<br>';
             }
+            echo '<p id="nombres">'.$texto.'</p>';
         }
+        echo '</div>';
     }
-    if (isset($_POST["nombre"]) && isset($_POST["mes"])) {
-        $mes = strtolower($_POST["mes"]);
-        echo "b".$mes."<br>";
-        $nombre = $_POST["nombre"];
-        $obj = new ejer6();
-        $obj->anadirNombre($nombre, $mes);
 
-
-    }
+    if(isset($_POST["nombre"]) && isset($_POST["mes"])) {
+            $mes = strtolower($_POST["mes"]);
+            if(array_key_exists($mes, $cumples)) {
+                $nombre = $_POST["nombre"];
+                $contador =anadirNombre($nombre, $mes);
+                mostrar();
+                echo 'Numero de personas:'.$contador;
+            } else {
+                echo '<p style="color: red">ESE MES NO EXISTE!</p>';
+            }
+    }else{
+        echo '<p style="color: red">No has introducido nada</p>';
+    } 
+    
+    
     ?>
-    <!--<pre>
-    <?php
-        print_r($_POST);
-    ?>
-    </pre>-->
 
 </body>
 
